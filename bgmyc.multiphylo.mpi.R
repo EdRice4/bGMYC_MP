e38fd9cb4d1658d29ad79c048f17a955ea300425
EdRice4 <edwinricethe4th@gmail.com>
2015-07-20 19:25:39 -0400

    Testing fugitive functionality

--- bgmyc.multiphylo.mpi.R
+++ bgmyc.multiphylo.mpi.R
@@ -1,3 +1,6 @@
+#!/usr/bin/env Rscript
+# This is a test.
+
 bgmyc.multiphylo.mpi <- function(
                                  multiphylo, mcmc, burnin, thinning, py1=0, py2=0,
                                  pc1=0, pc2=0, t1=2, t2=51, scale=c(20, 10, 5.00),
