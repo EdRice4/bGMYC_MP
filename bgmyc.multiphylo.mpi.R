dc90f77dd5e8bfb188e85bcebdd161b9edf3af23
EdRice4 <edwinricethe4th@gmail.com>
2015-07-21 18:06:42 -0400

    Added MPI version of bgmyc.gibbs that omits printing.

--- bgmyc.multiphylo.mpi.R
+++ bgmyc.multiphylo.mpi.R
@@ -4,7 +4,7 @@
 bgmyc.multiphylo.mpi <- function(
                                  multiphylo, mcmc, burnin, thinning, py1=0, py2=0,
                                  pc1=0, pc2=0, t1=2, t2=51, scale=c(20, 10, 5.00),
-                                 start=c(1.0, 0.5, 50.0), sampler=bgmyc.gibbs,
+                                 start=c(1.0, 0.5, 50.0), sampler=bgmyc.gibbs.mpi,
                                  likelihood=bgmyc.lik, prior=bgmyc.prior,
                                  nproc=NULL
                                  )
