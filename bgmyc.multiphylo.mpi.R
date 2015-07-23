cf3bc378acb41bdb95a678ab550de1b2dfcb4edb
EdRice4 <edwinricethe4th@gmail.com>
2015-07-23 16:03:38 -0400

    Removing unnecessary comments.

--- bgmyc.multiphylo.mpi.R
+++ bgmyc.multiphylo.mpi.R
@@ -1,6 +1,5 @@
 #!/usr/bin/env Rscript
 
-# Write that function, boi (I have no idea what I am doing)
 bgmyc.multiphylo.mpi <- function(
                                  multiphylo, mcmc, burnin, thinning, py1=0, py2=0,
                                  pc1=0, pc2=0, t1=2, t2=51, scale=c(20, 10, 5.00),
