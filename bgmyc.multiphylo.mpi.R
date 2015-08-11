2b6764bc97a7dac871447a5faf680f3b1e5480f5
EdRice4 <edwinricethe4th@gmail.com>
2015-08-11 15:34:18 -0400

    Fixed defaults for bgmyc.multiphylo.mpi.

--- bgmyc.multiphylo.mpi.R
+++ bgmyc.multiphylo.mpi.R
@@ -2,8 +2,8 @@
 
 bgmyc.multiphylo.mpi <- function(
                                  multiphylo, mcmc, burnin, thinning, py1=0,
-                                 py2=0, pc1=0, pc2=0, t1=2, t2=51,
-                                 scale=c(20, 10, 5.00), start=c(1.0, 0.5, 50.0),
+                                 py2=2, pc1=0, pc2=2, t1=2, t2=51,
+                                 scale=c(20, 10, 5), start=c(1, 0.5, 50),
                                  sampler=bgmyc.gibbs.mpi, likelihood=bgmyc.lik,
                                  prior=bgmyc.prior
                                  )
