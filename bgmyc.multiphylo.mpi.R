777b751b5f0e9553563747f3e0fe0b9c5ebaab05
EdRice4 <edwinricethe4th@gmail.com>
2015-08-11 13:35:16 -0400

    Modifed bgmyc.multiphylo.mpi.
    Formatting for columns.

--- bgmyc.multiphylo.mpi.R
+++ bgmyc.multiphylo.mpi.R
@@ -1,10 +1,11 @@
 #!/usr/bin/env Rscript
 
 bgmyc.multiphylo.mpi <- function(
-                                 multiphylo, mcmc, burnin, thinning, py1=0, py2=0,
-                                 pc1=0, pc2=0, t1=2, t2=51, scale=c(20, 10, 5.00),
-                                 start=c(1.0, 0.5, 50.0), sampler=bgmyc.gibbs.mpi,
-                                 likelihood=bgmyc.lik, prior=bgmyc.prior
+                                 multiphylo, mcmc, burnin, thinning, py1=0,
+                                 py2=0, pc1=0, pc2=0, t1=2, t2=51,
+                                 scale=c(20, 10, 5.00), start=c(1.0, 0.5, 50.0),
+                                 sampler=bgmyc.gibbs.mpi, likelihood=bgmyc.lik,
+                                 prior=bgmyc.prior
                                  )
 {
 
